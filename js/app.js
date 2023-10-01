1720608dc3b91ca635669fbaf1fb7a496fded407
Projeto Integrador 2023 <144556078+ProjetoIntegrador23@users.noreply.github.com>
2023-10-01 21:37:27 +0000

    Upload node modules e codigos adicionais

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const firebase = require('firebase/app');
+require('firebase/database'); 
+
+const config = {
+    apiKey: "AIzaSyBOlATv13ZOA_pmmCwfh-iH2EvuI7-PDuM",
+    authDomain: "meuconsumodeenergiaweb.firebaseapp.com",
+    databaseURL: "https://meuconsumodeenergiaweb-default-rtdb.firebaseio.com",
+    projectId: "meuconsumodeenergiaweb",
+    storageBucket: "meuconsumodeenergiaweb.appspot.com",
+    messagingSenderId: "376629492849",
+    appId: "1:376629492849:web:984c2d29d8427a42d6afd0",
+  };
+
+firebase.initializeApp(config);    
