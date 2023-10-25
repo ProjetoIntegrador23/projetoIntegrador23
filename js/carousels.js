9dfe5fc3bbfb473f1bc16dc3625e63e0c0997e4f
Giovana Oliveira <61835026+giovanaoliveira-14@users.noreply.github.com>
2023-10-25 01:21:36 +0000

    Upload de imagens e desenvolvimento do index

---
+++ js/carousels.js
@@ -0,0 +1,9 @@
+$('.componentes__items').slick({
+    slidesToShow: 5,
+    slidesToScroll: 3,
+    dots: false,
+    infinite: false,
+    arrow: true,
+    adaptiveHeight: true,
+    adaptiveWidth: true
+  });
